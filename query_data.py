da8bc1c16bb06d35e2c6cd77c46989f73ecaff19
236sakshi <108453602+236sakshi@users.noreply.github.com>
2026-05-11 08:45:38 +0530

    Clean up comments in query_data.py
    Removed commented lines to clean up the code.

--- query_data.py
+++ query_data.py
@@ -19,7 +19,7 @@ Answer the question based on the above context: {question}
 
 
 def main():
-    # Create CLI.
+   
     parser = argparse.ArgumentParser()
     parser.add_argument("query_text", type=str, help="The query text.")
     args = parser.parse_args()
@@ -28,17 +28,17 @@ def main():
 
 
 def query_rag(query_text: str):
-    # Prepare the DB.
+   
     embedding_function = get_embedding_function()
     db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
 
-    # Search the DB.
+   
     results = db.similarity_search_with_score(query_text, k=5)
 
     context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
     prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
     prompt = prompt_template.format(context=context_text, question=query_text)
-    # print(prompt)
+
 
     model = Ollama(model="phi3:mini")
     response_text = model.invoke(prompt)
